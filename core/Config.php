1df31b212fea69b81068d98a1f49eecc3ca9dd06
秋风悲画扇 <1069414136@qq.com>
2021-01-26 20:40:43 +0800

    自动载入

---
+++ core/Config.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace core;
+
+class Config
+{
+
+
+    /**
+     * 配置入口
+     */
+    public function run()
+    {
+
+        echo __CLASS__ . "配置入口 <hr/>";
+    }
+}
--- core/Frame.php
+++ core/Frame.php
@@ -6,21 +6,46 @@ namespace core;
 class Frame
 {
 
+    /** 存放已经引入的类 */
+    static $classMap = array();
+
+
     /**
      * 框架入口
      */
     public function run()
     {
         echo __CLASS__ . "框架入口 <hr/>";
+        $config = new Config();
+        $config->run();
     }
 
     /**
      * 自动载入
      * @param string $nameSpace
+     * @return bool
      */
-    public static function loader(string $nameSpace)
+    public static function loader(string $nameSpace): bool
     {
+        echo "自动载入: {$nameSpace} <hr/>";
 
-        echo __CLASS__ . "{$nameSpace} <hr/>";
+        /** 判断类是否已经引入 */
+        if (isset(self::$classMap[$nameSpace])) {
+            return true;
+        }
+
+        /** 组合路径 */
+        $class = str_replace('\\', '/', $nameSpace);
+        $classPath = appPath . $class . '.php';
+
+        /** 去除掉不存在的类文件 */
+        if (!is_file($classPath)) {
+            return false;
+        }
+
+        /** 引入类文件 */
+        include $classPath;
+        self::$classMap[$nameSpace] = $classPath;
+        return true;
     }
 }
