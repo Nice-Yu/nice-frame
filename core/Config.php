03df25d21807a8c83c92534b04bb281179464e92
秋风悲画扇 <tim-autumnwind@qiufengbeihuashandeMacBook-Pro.local>
2021-01-26 17:01:48 +0800

    v0.0.2 定义版本和加载配置

---
+++ core/Config.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace core;
+
+class Config
+{
+
+    /**
+     * 加载配置
+     */
+    public function run()
+    {
+        echo __CLASS__ . " 加载配置 " . PHP_EOL;
+    }
+
+}
